7efa559c1340f9063ab3c6a39b36a4e7188f5280
JaeHwan <wpghks56@naver.com>
2025-08-08 00:58:00 +0900

    add : bookmark 상태 추가에 따른 코드 변경

--- lib/presentation/screen/posts/post_view_model.dart
+++ lib/presentation/screen/posts/post_view_model.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/foundation.dart';
+import 'package:post_bookmark/bookmark/data_source/bookmark_data_source.dart';
+import 'package:post_bookmark/core/usecase/toggle_bookmark_usecase.dart';
 import 'package:post_bookmark/post/domain/model/post.dart';
 import 'package:post_bookmark/post/domain/respository/post_repository.dart';
 import 'package:post_bookmark/presentation/screen/posts/post_action.dart';
@@ -6,32 +8,48 @@ import 'package:post_bookmark/presentation/screen/posts/post_state.dart';
 
 class PostViewModel extends ChangeNotifier {
   final PostRepository _repository;
+  final BookmarkDataSource _bookmarkDataSource;
+  final ToggleBookmarkUsecase _usecase;
   PostState _state = const PostState();
+
+  PostViewModel({
+    required PostRepository repository,
+    required ToggleBookmarkUsecase usecase,
+    required BookmarkDataSource datasource,
+  }) : _repository = repository,
+       _usecase = usecase,
+       _bookmarkDataSource = datasource;
 
   PostState get state => _state;
 
-  PostViewModel({required PostRepository repository})
-    : _repository = repository;
-
-  Future<void> action(PostAction action) async {
+  Future<void> onAction(PostAction action) async {
     switch (action) {
       case TapPost():
         break;
       case TapBookMark():
-        await _tapBookMark();
+        await _tapBookMark(action.postId);
         break;
     }
   }
 
-  Future<void> _tapBookMark() async {}
+  Future<void> _tapBookMark(int postId) async {
+    final Set<String> bookmarks = await _usecase.execute(postId);
+    _state = state.copyWith(bookmarks: bookmarks);
+    notifyListeners();
+  }
 
   Future<void> init() async {
     _state = state.copyWith(isLoading: true);
     notifyListeners();
 
     try {
-      List<Post> posts = await _repository.findAll();
-      _state = state.copyWith(isLoading: false, posts: posts);
+      final List<Post> posts = await _repository.findAll();
+      final Set<String> bookmarks = await _bookmarkDataSource.findAll();
+      _state = state.copyWith(
+        isLoading: false,
+        posts: posts,
+        bookmarks: bookmarks,
+      );
       notifyListeners();
     } on Exception catch (e) {
       _state = state.copyWith(isLoading: false, error: e);
