a4cfbf76b31888eb5c0ec21eb3468c6bd357f7e2
JaeHwan <wpghks56@naver.com>
2025-08-08 00:58:39 +0900

    add : post detail state 에 bookmark 상태 추가

--- lib/presentation/screen/post_detail/post_detail_state.dart
+++ lib/presentation/screen/post_detail/post_detail_state.dart
@@ -7,11 +7,13 @@ part 'post_detail_state.freezed.dart';
 class PostDetailState with _$PostDetailState {
   final bool isLoading;
   final Post post;
+  final bool isBookmark;
   final Exception? error;
 
   const PostDetailState({
     this.isLoading = false,
     this.post = const Post(userId: 0, id: 0, title: 'N/A', body: 'N/A'),
     this.error,
+    this.isBookmark = false,
   });
 }
