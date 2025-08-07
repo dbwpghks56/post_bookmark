89fce8466199a4f1af744ea362af1ea4ddd75dfb
JaeHwan <wpghks56@naver.com>
2025-08-07 21:26:45 +0900

    add : http 추상화에 timeout 에러 핸들링 추가

--- lib/core/utils/http/data_source/impl/http_data_source_impl.dart
+++ lib/core/utils/http/data_source/impl/http_data_source_impl.dart
@@ -1,10 +1,16 @@
 import 'package:http/src/response.dart';
 import 'package:http/http.dart' as http;
+import 'package:post_bookmark/core/utils/errors/post_error.dart';
 import 'package:post_bookmark/core/utils/http/data_source/http_data_source.dart';
 
 class HttpDataSourceImpl implements HttpDataSource {
   @override
   Future<Response> get(Uri url, {Map<String, String>? headers}) async {
-    return http.get(url, headers: headers).timeout(const Duration(seconds: 10));
+    return http
+        .get(url, headers: headers)
+        .timeout(
+          const Duration(seconds: 10),
+          onTimeout: throw PostError.timeoutError,
+        );
   }
 }
