8a9746398862fbc74192693afe0f3b691608753f
JaeHwan <wpghks56@naver.com>
2025-08-07 20:13:16 +0900

    feature : post screen root 생성

---
+++ lib/presentation/screen/posts/screen/post_screen_root.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:post_bookmark/core/routing/routes.dart';
+import 'package:post_bookmark/core/utils/route_util.dart';
+import 'package:post_bookmark/presentation/screen/posts/post_action.dart';
+import 'package:post_bookmark/presentation/screen/posts/post_view_model.dart';
+import 'package:post_bookmark/presentation/screen/posts/screen/post_screen.dart';
+
+class PostScreenRoot extends StatelessWidget {
+  final PostViewModel viewModel;
+  const PostScreenRoot({super.key, required this.viewModel});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListenableBuilder(
+      listenable: viewModel,
+      builder: (context, _) {
+        return PostScreen(
+          state: viewModel.state,
+          onAction: (PostAction action) {
+            switch (action) {
+              case TapPost():
+                context.push(Routes.detail.changeIdtoValue(id: action.postId));
+              case TapBookMark():
+                // TODO: Handle this case.
+                throw UnimplementedError();
+            }
+          },
+        );
+      },
+    );
+  }
+}
