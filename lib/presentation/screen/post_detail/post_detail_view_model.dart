5b94a99ba2a4a0eef7b3c64025bd6e3a894a1006
JaeHwan <wpghks56@naver.com>
2025-08-07 20:13:44 +0900

    feature : post detail view model 생성

---
+++ lib/presentation/screen/post_detail/post_detail_view_model.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/foundation.dart';
+import 'package:post_bookmark/post/domain/model/post.dart';
+import 'package:post_bookmark/post/domain/respository/post_repository.dart';
+import 'package:post_bookmark/presentation/screen/post_detail/post_detail_state.dart';
+
+class PostDetailViewModel extends ChangeNotifier {
+  final PostRepository _repository;
+
+  PostDetailState _state = const PostDetailState();
+
+  PostDetailViewModel({required PostRepository repository})
+    : _repository = repository;
+
+  PostDetailState get state => _state;
+
+  Future<void> init({required int postId}) async {
+    _state = state.copyWith(isLoading: true);
+    notifyListeners();
+
+    Post post = await _repository.findByPostId(postId: postId);
+
+    _state = state.copyWith(isLoading: false, post: post);
+    notifyListeners();
+  }
+}
